36de4606532e2772fd6dd7980fcb0c94e1f1a09b
Sean Houde <seanhoude@gmail.com>
2019-01-04 18:56:05 -0500

    change api key and secure it using env variable

--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
+from django.conf import settings
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', { 'google_api_key': settings.GOOGLE_MAPS_API_KEY})
--- finalproject/settings.py
+++ finalproject/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('STUMBL_KEY')
-
+GOOGLE_MAPS_API_KEY = os.getenv('GOOGLE_MAPS_API_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
