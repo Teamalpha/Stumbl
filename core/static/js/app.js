18d84bb1ed827c678985c9d67abc82afeb2a8f17
Sean Houde <seanhoude@gmail.com>
2019-01-04 14:26:14 -0500

    Add google maps api

--- core/static/js/app.js
+++ core/static/js/app.js
@@ -1,2 +1,11 @@
 $(document).foundation();
 
+const apikey = config.API_KEY;
+
+var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+    }
