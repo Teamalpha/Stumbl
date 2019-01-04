41fa6b3bcf8115e153cc0dd26b4a46e2e80abdea
Sean Houde <seanhoude@gmail.com>
2019-01-04 14:45:42 -0500

    add tests

--- core/static/js/app.js
+++ core/static/js/app.js
@@ -9,3 +9,29 @@ var map;
           zoom: 8
         });
     }
+
+if (typeof Gyroscope === "function") {
+    console.log('The gyroscope is maybe possibly working??? ')
+}
+
+let magSensor = new Magnetometer({frequency: 60});
+
+magSensor.addEventListener('reading', e => {
+  console.log("Magnetic field along the X-axis " + magSensor.x);
+  console.log("Magnetic field along the Y-axis " + magSensor.y);
+  console.log("Magnetic field along the Z-axis " + magSensor.z);
+  console.log(e)
+})
+magSensor.addEventListener('error', event => {
+  console.log(event.error.name, event.error.message);
+})
+magSensor.start();
+
+navigator.permissions.query({name:'geolocation'}).then(function(result) {
+    if (result.state == 'granted') {
+      console.log('Hooray we have access!');
+    } else if (result.state == 'prompt') {
+      console.log('so sad, no permissions')
+    }
+    // Don't do anything if the permission was denied.
+  });
