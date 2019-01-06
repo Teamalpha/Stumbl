83578c031da62211791dbceb6796b5fb8aea581b
Sean Houde <seanhoude@gmail.com>
2019-01-06 13:33:15 -0500

    Center map to device location, add styling to map api

--- core/static/js/app.js
+++ core/static/js/app.js
@@ -2,26 +2,146 @@ $(document).foundation();
 
 // var apikey = config.API_KEY;
 
-var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 8
-        });
-    }
+var map, infoWindow;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 18,
+    styles: [
+      { elementType: 'geometry', stylers: [{ color: '#242f3e' }] },
+      { elementType: 'labels.text.stroke', stylers: [{ color: '#242f3e' }] },
+      { elementType: 'labels.text.fill', stylers: [{ color: '#746855' }] },
+      {
+        featureType: 'administrative.locality',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#d59563' }]
+      },
+      {
+        featureType: 'poi',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#d59563' }]
+      },
+      {
+        featureType: 'poi.park',
+        elementType: 'geometry',
+        stylers: [{ color: '#263c3f' }]
+      },
+      {
+        featureType: 'poi.park',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#6b9a76' }]
+      },
+      {
+        featureType: 'road',
+        elementType: 'geometry',
+        stylers: [{ color: '#38414e' }]
+      },
+      {
+        featureType: 'road',
+        elementType: 'geometry.stroke',
+        stylers: [{ color: '#212a37' }]
+      },
+      {
+        featureType: 'road',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#9ca5b3' }]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'geometry',
+        stylers: [{ color: '#746855' }]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'geometry.stroke',
+        stylers: [{ color: '#1f2835' }]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#f3d19c' }]
+      },
+      {
+        featureType: 'transit',
+        elementType: 'geometry',
+        stylers: [{ color: '#2f3948' }]
+      },
+      {
+        featureType: 'transit.station',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#d59563' }]
+      },
+      {
+        featureType: 'water',
+        elementType: 'geometry',
+        stylers: [{ color: '#17263c' }]
+      },
+      {
+        featureType: 'water',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#515c6d' }]
+      },
+      {
+        featureType: 'water',
+        elementType: 'labels.text.stroke',
+        stylers: [{ color: '#17263c' }]
+      }
+    ]
+  });
 
-$(window).resize(function() {
-    let mapWidth = $('#map').width();
-    $('#map').css({
-    'height': mapWidth + 'px'
+  infoWindow = new google.maps.InfoWindow;
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      console.log('navigator.geolocation worked!')
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      infoWindow.open(map);
+      map.setCenter(pos);
+    }, function () {
+      handleLocationError(true, infoWindow, map.getCenter());
     });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+    'Error: The Geolocation service failed.' :
+    'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
+}
+
+function roundify(selector = "#map") {
+  let mapWidth = $(selector).width();
+  $(selector).css({
+    'height': mapWidth + 'px'
+  });
+}
+
+$(document).ready(roundify('#map'))
+
+$(window).resize(function () {
+  let mapWidth = $('#map').width();
+  $('#map').css({
+    'height': mapWidth + 'px'
+  });
 })
 
 if (typeof Gyroscope === "function") {
-    console.log('The gyroscope is maybe possibly working??? ')
+  console.log('The gyroscope is maybe possibly working??? ')
 }
 
-let magSensor = new Magnetometer({frequency: 60});
+let magSensor = new Magnetometer({ frequency: 60 });
 
 magSensor.addEventListener('reading', e => {
   console.log("Magnetic field along the X-axis " + magSensor.x);
@@ -34,11 +154,11 @@ magSensor.addEventListener('error', event => {
 })
 magSensor.start();
 
-navigator.permissions.query({name:'geolocation'}).then(function(result) {
-    if (result.state == 'granted') {
-      console.log('Hooray we have access!');
-    } else if (result.state == 'prompt') {
-      console.log('so sad, no permissions')
-    }
-    // Don't do anything if the permission was denied.
-  });
+navigator.permissions.query({ name: 'geolocation' }).then(function (result) {
+  if (result.state == 'granted') {
+    console.log('Hooray we have access!');
+  } else if (result.state == 'prompt') {
+    console.log('so sad, no permissions')
+  }
+  // Don't do anything if the permission was denied.
+});
