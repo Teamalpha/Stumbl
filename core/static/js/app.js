92c34414a34204bdedf11c15e5ec76c06d537b9e
Sean Houde <seanhoude@gmail.com>
2019-01-07 23:14:39 -0500

    add svg

--- core/static/js/app.js
+++ core/static/js/app.js
@@ -1,15 +1,29 @@
 $(document).foundation();
 
+var originX, originY, radius;
+
+var compass = $('#cir')
+
 function roundify(selector="#map") {
   let mapWidth = $(selector).width();
   $(selector).css({
     'height': mapWidth + 'px'
   });
   var mapOffset = $(selector).offset()
-  var radius = mapWidth / 2
-  var originX = mapOffset.top + radius
-  var originY = mapOffset.left + radius
+  radius = mapWidth / 2
+  console.log(`before -- x: ${mapOffset.left}, y: ${mapOffset.top}, r: ${radius}`)
+  console.log('Map Width: ', mapWidth)
+  console.log('Map Height: ', $(selector).height())
+
+  originX = mapOffset.left + radius
+  originY = radius + mapOffset.top
   console.log(`x: ${originX}, y: ${originY}`)
+  compass.attr('cx', originX.toString())
+  compass.attr('cy', originY.toString())
+  compass.attr('r', (radius * 1.1).toString())
+  console.log('radius: ', compass.attr('r'))
+  console.log('x: ', compass.attr('cx'))
+  console.log('y: ', compass.attr('cy'))
 }
 
 $(document).ready(roundify())
@@ -17,6 +31,7 @@ $(document).ready(roundify())
 $(window).resize(function () {
   roundify()
 })
+
 
 
 // setInterval(function() {
