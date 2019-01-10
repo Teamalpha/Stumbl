6d91aca79b76ebca3c792ae258daf1ea208e070a
Sean Houde <seanhoude@gmail.com>
2019-01-10 08:32:27 -0500

    Add fulltilt.js for cross compatibility, integrated compass and bearing

--- core/static/js/app.js
+++ core/static/js/app.js
@@ -6,7 +6,7 @@ let bearing = 0
 let mapDiv = $('#map')
 
 function roundify() {
-  mapDiv.css({'height': mapDiv.width() + 'px'});
+  mapDiv.css({ 'height': mapDiv.width() + 'px' });
   radius = mapDiv.width() / 2
   originX = mapDiv.offset().left + radius
   originY = mapDiv.offset().top + radius
@@ -21,26 +21,62 @@ function roundify() {
   // ` 
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
   roundify()
 })
 $(window).resize(function () {
   roundify()
 })
 
-$('.slider').on('moved.zf.slider', function(){
-  let degrees = 
-  arrow.style.cssText = `
-    transform: rotate(${$(this).children('.slider-handle').attr('aria-valuenow')}deg);
-    transform-origin: bottom center;
-    height: ${radius + 60}px;
-    padding-bottom: ${radius - 35}px;
-    top: ${originY - 60 - radius}px;
-    left: ${originX - 30}px;
-    display: inline-block;
-    `
-});
+// $('.slider').on('moved.zf.slider', function () {
+//   let degrees =
+//     arrow.style.cssText = `
+//     transform: rotate(${$(this).children('.slider-handle').attr('aria-valuenow')}deg);
+//     transform-origin: bottom center;
+//     height: ${radius + 60}px;
+//     padding-bottom: ${radius - 35}px;
+//     top: ${originY - 60 - radius}px;
+//     left: ${originX - 30}px;
+//     display: inline-block;
+//     `
+// });
 
-// setInterval(function() {
-//   console.log(updatebearing)
-//   }, 5000)
+setInterval(getMySpot, 1000);
+
+// Obtain a new *world-oriented* Full Tilt JS DeviceOrientation Promise
+var promise = FULLTILT.getDeviceOrientation({ 'type': 'world' });
+
+// Wait for Promise result
+promise.then(function (deviceOrientation) { // Device Orientation Events are supported
+
+  // Register a callback to run every time a new 
+  // deviceorientation event is fired by the browser.
+  deviceOrientation.listen(function () {
+
+    // Get the current *screen-adjusted* device orientation angles
+    var currentOrientation = deviceOrientation.getScreenAdjustedEuler();
+
+    // Calculate the current compass heading that the user is 'looking at' (in degrees)
+    var compassHeading = 360 - currentOrientation.alpha;
+    // $('#c-heading').text(Math.abs(compassHeading))
+    // $('#c-bearing').text(Math.abs(updatebearing))
+    arrow.style.cssText = `
+      transform: rotate(${Math.abs(compassHeading - updatebearing)}deg);
+      transform-origin: bottom center;
+      height: ${radius + 60}px;
+      padding-bottom: ${radius - 35}px;
+      top: ${originY - 60 - radius}px;
+      left: ${originX - 30}px;
+      display: inline-block;
+    `
+    // Do something with `compassHeading` here...
+
+  });
+
+}).catch(function (errorMessage) { // Device Orientation Events are not supported
+
+  console.log(errorMessage);
+
+  // Implement some fallback controls here...
+
+});
--- core/static/js/compass.js
+++ core/static/js/compass.js
@@ -1,10 +1,10 @@
 var updatebearing;
 
-function getMySpot () {
-    navigator.geolocation.getCurrentPosition(function(position) {
+function getMySpot() {
+    navigator.geolocation.getCurrentPosition(function (position) {
         const location = document.getElementById('location');
         // const location2 = document.getElementById('location2');
-        location.innerText = `My coordinates are: ${position.coords.latitude}, ${position.coords.longitude}`
+        // location.innerText = `My coordinates are: ${position.coords.latitude}, ${position.coords.longitude}`
         loNg = position.coords.longitude
         laT = position.coords.latitude
         if (placeCoords) {
@@ -16,29 +16,29 @@ function getMySpot () {
 
 getMySpot()
 
-function whichWaytoGo () { 
+function whichWaytoGo() {
     var mySpot = Math.round(getBearing(toRadian(laT), toRadian(loNg), toRadian(placeCoords.lat), toRadian(placeCoords.lng)))
     compasser = document.getElementById('alpha');
-    compasser.innerText = `My heading is: ${mySpot} degrees`;
+    // compasser.innerText = `My heading is: ${mySpot} degrees`;
     return mySpot
 }
 
 
 function calcAngle(opposite, adjacent) {
     return Math.atan2(opposite, adjacent);
-  }
+}
 
-function toDegrees (angle) {
+function toDegrees(angle) {
     return angle * (180 / Math.PI);
-  }
+}
 
-function toRadian (degree) {
+function toRadian(degree) {
     return degree * Math.PI / 180;
 }
 
 function getBearing(lat1, long1, lat2, long2) {
-    var y = Math.sin(long2-long1) * Math.cos(lat2);
-    var x = Math.cos(lat1) * Math.sin(lat2) - Math.sin(lat1) * Math.cos(lat2) * Math.cos(long2-long1);
+    var y = Math.sin(long2 - long1) * Math.cos(lat2);
+    var x = Math.cos(lat1) * Math.sin(lat2) - Math.sin(lat1) * Math.cos(lat2) * Math.cos(long2 - long1);
     var brng = Math.atan2(y, x);
     var brngInDegs = toDegrees(brng);
     if (brngInDegs < 0) {
@@ -47,41 +47,40 @@ function getBearing(lat1, long1, lat2, long2) {
     return brngInDegs;
 }
 
-function compassAnimation (angler) {
-    const animate = document.getElementById('canvas');
-    const arrow = document.getElementById('source');
-    const ctx = animate.getContext('2d');
-    const direction = (angler * Math.PI / 180);
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.translate(150, 150);
-    // ctx.rotate(-(direction + Math.round(updatebearing)));
-    // ctx.rotate(toRadian(-(angler + Math.round(updatebearing))));
-    ctx.rotate(-(direction + toRadian(updatebearing)))
-    ctx.translate(-150, -150)
-    ctx.drawImage(arrow, 50, 50)
-    ctx.resetTransform();
- }
+// function compassAnimation (angler) {
+//     const animate = document.getElementById('canvas');
+//     const arrow = document.getElementById('source');
+//     const ctx = animate.getContext('2d');
+//     const direction = (angler * Math.PI / 180);
+//     ctx.clearRect(0, 0, canvas.width, canvas.height);
+//     ctx.translate(150, 150);
+//     // ctx.rotate(-(direction + Math.round(updatebearing)));
+//     // ctx.rotate(toRadian(-(angler + Math.round(updatebearing))));
+//     ctx.rotate(-(direction + toRadian(updatebearing)))
+//     ctx.translate(-150, -150)
+//     ctx.drawImage(arrow, 50, 50)
+//     ctx.resetTransform();
+//  }
 
-if (window.DeviceOrientationEvent) {
-    window.addEventListener("deviceorientation", function(event) {
-        document.getElementById("test").innerText = 'supported!'
-        
-        var compassHeading = Math.round(event.webkitCompassHeading);
+// if (window.DeviceOrientationEvent) {
+//     window.addEventListener("deviceorientation", function(event) {
+//         document.getElementById("test").innerText = 'supported!'
 
-        document.getElementById('compass').innerText = `the input is ${compassHeading}`;
+//         var compassHeading = Math.round(event.webkitCompassHeading);
 
-        // setInterval(getMySpot, 3000);
-        document.getElementById('arrow').style.cssText = `
-            transform: rotate(${updatebearing}deg);
-            transform-origin: bottom center;
-            height: ${radius + 60}px;
-            padding-bottom: ${radius - 35}px;
-            top: ${originY - 60 - radius}px;
-            left: ${originX - 30}px;
-            display: inline-block;
-        ` 
-        console.log(updatebearing)
-    }, true);
-}
+//         document.getElementById('compass').innerText = `the input is ${compassHeading}`;
 
-setInterval(getMySpot, 1000);
+//         // setInterval(getMySpot, 3000);
+//         document.getElementById('arrow').style.cssText = `
+//             transform: rotate(${updatebearing}deg);
+//             transform-origin: bottom center;
+//             height: ${radius + 60}px;
+//             padding-bottom: ${radius - 35}px;
+//             top: ${originY - 60 - radius}px;
+//             left: ${originX - 30}px;
+//             display: inline-block;
+//         ` 
+//         console.log(updatebearing)
+//     }, true);
+// }
+
