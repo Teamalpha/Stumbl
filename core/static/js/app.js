5b5a66e7b08ace9cbb35d6d034e7c0aca15906e7
Sean Houde <seanhoude@gmail.com>
2019-01-12 11:48:40 -0500

    Add vue.js, set up playlist modals and click handlers

--- core/static/js/app.js
+++ core/static/js/app.js
@@ -1,10 +1,7 @@
 
-let originX, originY, radius, flag;
+let originX, originY, radius;
 let arrow = document.getElementById('arrow')
 let mapDiv = $('#map')
-let cities = []
-let cityPlaylists = []
-let destinations = []
 
 function roundify() {
   mapDiv.css({ 'height': mapDiv.width() + 'px' });
@@ -27,10 +24,10 @@ $(window).resize(function () {
 setInterval(getMySpot, 200);
 
 // Set compass arrow for Android Devices
-window.ondeviceorientationabsolute = function(event) {
+window.ondeviceorientationabsolute = function (event) {
   compassHeading = event.alpha
   var finalHeading = compassHeading + updatebearing
-  if (finalHeading > 360) { finalHeading -= 360}
+  if (finalHeading > 360) { finalHeading -= 360 }
   $('#c-heading').text(`Absolute direction: ${compassHeading}`)
   $('#c-bearing').text(`Destination_______: ${updatebearing}`)
   $('#c-finalheading').text(`Final Destination : ${finalHeading}`)
@@ -41,12 +38,12 @@ window.ondeviceorientationabsolute = function(event) {
 
 // set compass arrow for iOs devices
 if (window.DeviceOrientationEvent) {
-  window.addEventListener("deviceorientation", function(event) {
+  window.addEventListener("deviceorientation", function (event) {
     if (event.webkitCompassHeading) {
       roundify()
       compassHeading = event.webkitCompassHeading
-      var finalHeading = updatebearing - compassHeading 
-      if (finalHeading < 0) { finalHeading += 360}
+      var finalHeading = updatebearing - compassHeading
+      if (finalHeading < 0) { finalHeading += 360 }
       $('#c-heading').text(`Absolute direction: ${compassHeading}`)
       $('#c-bearing').text(`Destination_______: ${updatebearing}`)
       $('#c-finalheading').text(`Final Destination : ${finalHeading}`)
@@ -56,61 +53,55 @@ if (window.DeviceOrientationEvent) {
   })
 }
 
-function getCities() {
-  for (let playlist of playlists) {
-    if (!cities.includes(playlist.city)) {
-      cities.push(playlist.city)
-    }
-  }
-  return cities
-}
 
-$('.open-pl-modal').click(function() {
-  $.get("/api/playlists/", function(data){
-    playlists = data
-  }).then(() => {
-    cities = getCities()
-    $('#city-list').empty()
-    // Generate list of cities
-    for (let city of cities) {
-      $('#city-list').append(`<div><button id="${city}" class="button is-primary">${city}</button></div>`)
-      // click handler for city
-      $(`#${city}`).click(function() {
-        $.get(`/api/playlists/?search=${city}`, function(data) {
-          cityPlaylists = data
-        }).then(() => {
-          $('#city-playlists').empty()
-          // Generate list of playlists
-          for (let playlist of cityPlaylists) {
-            $('#city-playlists').append(`<div><button id="${playlist.title}" class="button is-primary">${playlist.title}</button></div>`)
-            // flag = true
-            // console.log(flag)
-            // // click handler for playlist
-            // $(`#${playlist.title}`).click(function() {
-            //   alert(playlist.pk)
-            //   $.get(`/api/playlists/${playlist.pk}`, function(data) {
-            //     destinations = data
-            //     console.log('playlist clicked!')
-            //   }).then(() => {
-            //     $('#playlist-detail').empty()
-            //     // Generate list of destinations
-            //     for (let destination of destinations) {
-            //       $('#playlist-detail').append(`<div><button id="${destination.name}" class="button is-primary">${destination.name}</button></div>`)
-            //     }
-            //     $('#playlist-detail-modal').addClass('is-active')
-            //   })
-            // })
-          }
-          $('#city-playlists-modal').addClass('is-active')
-        })
-      })
-    }
-    $('#choose-city-modal').addClass('is-active')
-  })
+$('.open-pl-modal').click(function () {
+  $('#choose-city-modal').addClass('is-active')
 })
+//   $.get("/api/playlists/", function(data){
+//     playlists = data
+//   }).then(() => {
+//     cities = getCities()
+//     $('#city-list').empty()
+//     // Generate list of cities
+//     for (let city of cities) {
+//       $('#city-list').append(`<div><button id="${city}" class="button is-primary">${city}</button></div>`)
+//       // click handler for city
+//       $(`#${city}`).click(function() {
+//         $.get(`/api/playlists/?search=${city}`, function(data) {
+//           cityPlaylists = data
+//         }).then(() => {
+//           $('#city-playlists').empty()
+//           // Generate list of playlists
+//           for (let playlist of cityPlaylists) {
+//             $('#city-playlists').append(`<div><button id="${playlist.title}" class="button is-primary">${playlist.title}</button></div>`)
+// flag = true
+// console.log(flag)
+// // click handler for playlist
+// $(`#${playlist.title}`).click(function() {
+//   alert(playlist.pk)
+//   $.get(`/api/playlists/${playlist.pk}`, function(data) {
+//     destinations = data
+//     console.log('playlist clicked!')
+//   }).then(() => {
+//     $('#playlist-detail').empty()
+//     // Generate list of destinations
+//     for (let destination of destinations) {
+//       $('#playlist-detail').append(`<div><button id="${destination.name}" class="button is-primary">${destination.name}</button></div>`)
+//     }
+//     $('#playlist-detail-modal').addClass('is-active')
+//   })
+// })
+// }
+// $('#city-playlists-modal').addClass('is-active')
+//         })
+//       })
+//     }
+
+//   })
+// })
 
 function exitButtonListener(buttonSelector, modalSelector) {
-  $(buttonSelector).click(function() {
+  $(buttonSelector).click(function () {
     $(modalSelector).removeClass('is-active')
   })
 }
@@ -118,14 +109,3 @@ function exitButtonListener(buttonSelector, modalSelector) {
 exitButtonListener('#exit-city-playlists-modal', '#city-playlists-modal')
 exitButtonListener('#exit-choose-city-modal', '#choose-city-modal')
 exitButtonListener('#exit-playlist-detail-modal', '#playlist-detail-modal')
-
-// setInterval(function () {
-// if (flag) {
-//   for (let playlist of cityPlaylists) {
-//     $(`#${playlist.title}`).click(function() {
-//       console.log(`you clicked ! `)
-//     })
-//   }
-//   console.log(cityPlaylists)
-// flag = false
-// }}, 1000)
--- core/static/js/vueinstance.js
+++ core/static/js/vueinstance.js
@@ -4,48 +4,64 @@
 // const requestUserPk = parseInt(document.getElementById('request-user-pk').value) || -1
 // const requestUser = document.getElementById('request-user').value
 
-const vm = new VTTCue({
-    el: '#modalDiv',
-    delimiters: ['${', '}'],
-    data: {
-      playlists = [],
-      cityPlaylists: [],
-      cities: [],
-      loggedInUser: { 'followers': [], 'pk': -1, 'url': null, 'username': requestUser, 'users_followed': [] },
-      newResponse: { 'text': null, 'post': null, 'user': requestUser },
-      currentPost: {},
-      newPost: { 'text': null },
-      newFollow: { 'following_user': requestUserPk, 'followed_user': null },
-    },
-    mounted: function () {
+const vm = new Vue({
+  el: '#vue-instance',
+  delimiters: ['${', '}'],
+  data: {
+    playlist: [],
+    cityPlaylists: [],
+    cities: [],
+    currentPlaylist: [],
+    // loggedInUser: { 'followers': [], 'pk': -1, 'url': null, 'username': requestUser, 'users_followed': [] },
+    // newResponse: { 'text': null, 'post': null, 'user': requestUser },
+    // currentPost: {},
+    // newPost: { 'text': null },
+    // newFollow: { 'following_user': requestUserPk, 'followed_user': null },
+  },
+  mounted: function () {
     //   if (requestUserPk !== -1) {
     //     this.getLoggedInUser();
     //   } else {
     //     this.showFeedNotAll = false;
     //   }
     //   this.getPosts();
+    this.getCities()
+  },
+  methods: {
+    getCities: function () {
+      this.$http.get("/api/playlists/").then((response) => {
+        this.playlists = response.data;
+        for (let playlist of this.playlists) {
+          if (!this.cities.includes(playlist.city)) {
+            this.cities.push(playlist.city)
+          }
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      })
     },
-    methods: {
-      getCities: function () {
-        displayedItems = 0
-        this.$http.get("/api/playlists/").then((response) => {
-          
-          this.cityPlaylists = response.data;
-          this.showPostsNotUsers = true;
-        })
-          .catch((err) => {
-            console.log(err);
-          })
-      },
-    }
-  
-})
+    getCityPlaylists: function (city) {
+      this.$http.get(`/api/playlists/?search=${city}`).then((response) => {
+        this.cityPlaylists = response.data;
+        document.getElementById('city-playlists-modal').classList.add('is-active')
+        console.log(this.cityPlaylists)
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+    },
+    getPlaylist: function (playlist) {
+      this.$http.get(`/api/playlists/${playlist.pk}`).then((response) => {
+        this.currentPlaylist = response.data;
+        document.getElementById('playlist-detail-modal').classList.add('is-active')
+        console.log(this.currentPlaylist)
+      })
+    },
+  }, // close methods
+}) // close vue instance
 
 function getCities() {
-  for (let playlist of playlists) {
-    if (!cities.includes(playlist.city)) {
-      cities.push(playlist.city)
-    }
-  }
+
   return cities
 }
