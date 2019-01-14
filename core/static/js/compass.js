ee8d949ba8bd9b399f5e1a71ef3de82ca241b58b
Peter Oas <oas.peter@gmail.com>
2019-01-14 12:27:25 -0500

    distance to destination listed in on the bottom of the page, in KMs

--- core/static/js/compass.js
+++ core/static/js/compass.js
@@ -2,15 +2,17 @@ var updatebearing;
 
 function getMySpot() {
     navigator.geolocation.getCurrentPosition(function (position) {
-        const location = document.getElementById('location');
+        const distance = document.getElementById('c-distance');
         // const location2 = document.getElementById('location2');
         // location.innerText = `My coordinates are: ${position.coords.latitude}, ${position.coords.longitude}`
         loNg = position.coords.longitude
         laT = position.coords.latitude
         if (placeCoords) {
             updatebearing = whichWaytoGo()
+            updatedistance = getDistance(laT, loNg, placeCoords.lat, placeCoords.lng)
+            distance.innerText = `Destination Distance: ${updatedistance}`
         }
-        // location2.innerText = `tracker says: ${updatebearing}`
+
     })
 }
 
@@ -46,3 +48,33 @@ function getBearing(lat1, long1, lat2, long2) {
     }
     return brngInDegs;
 }
+
+// function getDistance(lat1, lon1, lat2, lon2) {
+//     const R = 6371;
+//     let l1 = toRadian(lat1);
+//     let l2 = toRadian(lat2);
+//     let cl = toRadian(lat2-lat1);
+//     let cλ = toRadian(lon2-lon1);
+
+//     let a = Math.sin(cl/2) * Math.sin(cl/2) +
+//             Math.cos(l1) * Math.cos(l2) *
+//             Math.sin(cλ/2) * Math.sin(cλ/2);
+//     let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+//     let d = (R * 100) * c;
+//     return d;
+// }
+
+function getDistance(lat1,lon1,lat2,lon2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = toRadian(lat2-lat1);  // toRadian below
+    var dLon = toRadian(lon2-lon1); 
+    var a = 
+      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(toRadian(lat1)) * Math.cos(toRadian(lat2)) * 
+      Math.sin(dLon/2) * Math.sin(dLon/2)
+      ; 
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    var d = R * c; // Distance in km
+    return d;
+  }
